0bcd744de6f98cba09ec54d6dfa66ab9da88e1f6
Georgy Chernyshov <georgy.chernyshov@gmail.com>
2020-08-29 20:42:17 +0300

    Checking git hook work

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Console;
 
 use App\Console\Commands\Example;
-use Illuminate\Console\Scheduling\Schedule;
 use Ctl\Console\Kernel as ConsoleKernel;
+use Illuminate\Console\Scheduling\Schedule;
 
 class Kernel extends ConsoleKernel
 {
